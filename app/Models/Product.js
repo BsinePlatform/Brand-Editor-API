24697a114f248a601440182e32e22cdfa8f570cb
Daywison <daywison.leal@gmail.com>
2019-12-17 17:41:13 -0300

    Criado o model Images

--- app/Models/Product.js
+++ app/Models/Product.js
@@ -4,6 +4,14 @@
 const Model = use('Model')
 
 class Product extends Model {
+    
+    subcategory () {
+        return this.belongsTo('App/Models/SubCategory')
+    }
+
+    users () {
+        return this.hasOne('App/Models/User')
+    }
 }
 
 module.exports = Product
