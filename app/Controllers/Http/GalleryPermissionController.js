f645e2cc61b8cf817a135ca11f2f0bf7a070d1a2
Daywison <daywison.leal@gmail.com>
2020-01-08 10:25:04 -0300

    Criado o Model Gallery Permission

---
+++ app/Controllers/Http/GalleryPermissionController.js
@@ -0,0 +1,130 @@
+'use strict'
+
+const GalleryPermission = use('App/Models/GalleryPermission')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with gallerypermissions
+ */
+class GalleryPermissionController {
+  /**
+   * Show a list of all gallerypermissions.
+   * GET gallerypermissions
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const galleryPermission = await GalleryPermission.all()
+    return galleryPermission
+  }
+
+  /**
+   * Render a form to be used for creating a new gallerypermission.
+   * GET gallerypermissions/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new gallerypermission.
+   * POST gallerypermissions
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    const data = request.only([
+      "id_gallery",
+      "id_user",
+      "id_department",
+      "id_store",
+      "id_company",
+    ])
+    
+    const galleryPermission = await GalleryPermission.create(data)
+    return galleryPermission
+  }
+
+  /**
+   * Display a single gallerypermission.
+   * GET gallerypermissions/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+    const galleryPermission = await GalleryPermission.findOrFail(params.id)
+    await galleryPermission.load('gallery')
+    await galleryPermission.load('user')
+    await galleryPermission.load('department')
+    await galleryPermission.load('store')
+    await galleryPermission.load('company')
+
+    return galleryPermission
+  }
+
+  /**
+   * Render a form to update an existing gallerypermission.
+   * GET gallerypermissions/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update gallerypermission details.
+   * PUT or PATCH gallerypermissions/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+    const galleryPermission = await GalleryPermission.findOrFail(params.id)
+    const data = request.only([
+      "id_gallery",
+      "id_user",
+      "id_department",
+      "id_store",
+      "id_company"
+    ])
+
+    galleryPermission.merge(data)
+    await galleryPermission.save()
+    return galleryPermission
+    
+  }
+
+  /**
+   * Delete a gallerypermission with id.
+   * DELETE gallerypermissions/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+    const galleryPermission = await GalleryPermission.findOrFail(params.id)
+    await galleryPermission.delete()    
+  }
+}
+
+module.exports = GalleryPermissionController
